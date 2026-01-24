c03c7cb879aa859843ae7796328ae1beda76daee
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-24 21:28:42 +0200

    Initialized testing project.

---
+++ src/test/java/dev/iraelie/testing/TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.iraelie.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
