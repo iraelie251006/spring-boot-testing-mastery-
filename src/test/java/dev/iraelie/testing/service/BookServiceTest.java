ef86ba4610cd73e1f4667a9b86d6c85b2d2faa47
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-25 21:23:01 +0200

    Extended Mockito extension & Created required mock objects.

--- src/test/java/dev/iraelie/testing/service/BookServiceTest.java
+++ src/test/java/dev/iraelie/testing/service/BookServiceTest.java
@@ -1,7 +1,21 @@
 package dev.iraelie.testing.service;
 
-import org.junit.jupiter.api.Test;
+import dev.iraelie.testing.mapper.BookMapper;
+import dev.iraelie.testing.repository.AuthorRepository;
+import dev.iraelie.testing.repository.BookRepository;
+import dev.iraelie.testing.repository.PublisherRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class BookServiceTest {}
+@ExtendWith(MockitoExtension.class)
+class BookServiceTest {
+    @Mock
+    private BookRepository bookRepository;
+    @Mock
+    private AuthorRepository authorRepository;
+    @Mock
+    private PublisherRepository publisherRepository;
+    @Mock
+    private BookMapper bookMapper;
+}
