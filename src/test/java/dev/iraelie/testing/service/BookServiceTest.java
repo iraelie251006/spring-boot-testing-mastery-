4b9b3d1c96c5c41a8b39167060571e888955b2d4
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-25 23:14:37 +0200

    Created test data for Author, Book, and Publisher.

--- src/test/java/dev/iraelie/testing/service/BookServiceTest.java
+++ src/test/java/dev/iraelie/testing/service/BookServiceTest.java
@@ -7,6 +7,7 @@ import dev.iraelie.testing.dtos.UpdateBookRequest;
 import dev.iraelie.testing.mapper.BookMapper;
 import dev.iraelie.testing.model.Author;
 import dev.iraelie.testing.model.Book;
+import dev.iraelie.testing.model.BookGenre;
 import dev.iraelie.testing.model.Publisher;
 import dev.iraelie.testing.repository.AuthorRepository;
 import dev.iraelie.testing.repository.BookRepository;
@@ -16,6 +17,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
 
 @ExtendWith(MockitoExtension.class)
 class BookServiceTest {
@@ -41,6 +44,33 @@ class BookServiceTest {
 
     @BeforeEach
     void setup() {
+        this.author = Author.builder()
+                .id(2L)
+                .firstName("John")
+                .lastName("Doe")
+                .email("john@gmail.com")
+                .country("Rwanda")
+                .books(new ArrayList<>())
+                .build();
 
+        this.publisher = Publisher.builder()
+                .id(3L)
+                .name("Amazon")
+                .address("USA")
+                .website("https://amazon.com")
+                .books(new ArrayList<>())
+                .build();
+
+        this.book = Book.builder()
+                .id(1L)
+                .title("Master System design")
+                .isbn("5155172381")
+                .publicationYear(2026)
+                .availableCopies(200)
+                .price(49.9)
+                .genre(BookGenre.valueOf("SCIENCE"))
+                .author(this.author)
+                .publisher(this.publisher)
+                .build();
     }
 }
