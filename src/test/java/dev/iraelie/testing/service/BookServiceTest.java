df646edeaf291e4d0eb48112539be2cf104a2523
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-25 21:40:51 +0200

    Injected mocks & prepared fields to set some data for.

--- src/test/java/dev/iraelie/testing/service/BookServiceTest.java
+++ src/test/java/dev/iraelie/testing/service/BookServiceTest.java
@@ -1,10 +1,19 @@
 package dev.iraelie.testing.service;
 
+import dev.iraelie.testing.dtos.AuthorDTO;
+import dev.iraelie.testing.dtos.BookDTO;
+import dev.iraelie.testing.dtos.CreateBookRequest;
+import dev.iraelie.testing.dtos.UpdateBookRequest;
 import dev.iraelie.testing.mapper.BookMapper;
+import dev.iraelie.testing.model.Author;
+import dev.iraelie.testing.model.Book;
+import dev.iraelie.testing.model.Publisher;
 import dev.iraelie.testing.repository.AuthorRepository;
 import dev.iraelie.testing.repository.BookRepository;
 import dev.iraelie.testing.repository.PublisherRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -18,4 +27,20 @@ class BookServiceTest {
     private PublisherRepository publisherRepository;
     @Mock
     private BookMapper bookMapper;
+
+    @InjectMocks
+    private BookService bookService;
+
+    private Author author;
+    private Publisher publisher;
+    private Book book;
+    private BookDTO bookDTO;
+    private AuthorDTO authorDTO;
+    private CreateBookRequest createBookRequest;
+    private UpdateBookRequest updateBookRequest;
+
+    @BeforeEach
+    void setup() {
+
+    }
 }
