492fd6215f8917aa7e487685faa9cc4ebcaa83f1
Elie <niyubwayoiraelie5777@gmail.com>
2026-02-11 23:32:05 +0200

    imported JpaRepository in UserRepository

--- src/main/java/dev/iraelie/testing/repository/UserRepository.java
+++ src/main/java/dev/iraelie/testing/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package dev.iraelie.testing.repository;
 
-public interface UserRepository {
+import dev.iraelie.testing.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
 }
