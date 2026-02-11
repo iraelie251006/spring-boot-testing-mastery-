a7115c9d1669fd3c76e249a4f8ec2998aaa4722c
Elie <niyubwayoiraelie5777@gmail.com>
2026-02-11 23:35:53 +0200

    added existsByEmailIgnoreCase method to be implemented.

--- src/main/java/dev/iraelie/testing/repository/UserRepository.java
+++ src/main/java/dev/iraelie/testing/repository/UserRepository.java
@@ -4,4 +4,5 @@ import dev.iraelie.testing.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    boolean existsByEmailIgnoreCase(String email);
 }
