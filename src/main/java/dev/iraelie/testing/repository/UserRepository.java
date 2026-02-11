8a4b51e9b56fcb6fab2949d4362d991dd63562d1
Elie <niyubwayoiraelie5777@gmail.com>
2026-02-11 23:37:08 +0200

    added existsByPhoneNumber method to be implemented.

--- src/main/java/dev/iraelie/testing/repository/UserRepository.java
+++ src/main/java/dev/iraelie/testing/repository/UserRepository.java
@@ -9,4 +9,6 @@ public interface UserRepository extends JpaRepository<User, String> {
     boolean existsByEmailIgnoreCase(String email);
 
     Optional<User> findByEmailIgnoreCase(String email);
+
+    boolean existsByPhoneNumber(String phoneNumber);
 }
