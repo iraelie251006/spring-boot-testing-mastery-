82fac3f9cd7207ba3ce82e11a2c614c1b96d803e
Elie <niyubwayoiraelie5777@gmail.com>
2026-02-12 22:33:54 +0200

    Implemented BusinessException class and its constructor built.

--- src/main/java/dev/iraelie/testing/exception/BusinessException.java
+++ src/main/java/dev/iraelie/testing/exception/BusinessException.java
@@ -1,4 +1,16 @@
 package dev.iraelie.testing.exception;
 
-public class BusinessException {
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+    private final Object[] args;
+
+    public BusinessException(final ErrorCode errorCode, final Object... args) {
+        super(getFormatterMessage(errorCode, args));
+        this.errorCode = errorCode;
+        this.args = args;
+    }
 }
